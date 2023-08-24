2d3b38fa49d5edaaf1603b4eb48e46f93a72dba1
EliasGalasco <persyxd@gmail.com>
2023-08-24 09:43:38 -0300

    agregando login

---
+++ login/login.js
@@ -0,0 +1,8 @@
+function iniciarSesion() {
+    let usuario = prompt("Ingrese su nombre de usuario:");
+    let contraseña = prompt("Ingrese su contraseña:");
+
+    alert("¡Inicio de sesión exitoso!");
+    window.location.href = "./login.html";
+    }
+
