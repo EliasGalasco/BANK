daee8a9ff68a7465f4e09edb4e619f8a9632affb
EliasGalasco <persyxd@gmail.com>
2023-08-28 03:40:57 -0300

    agregue funciones js 1.0

--- style.js
+++ style.js
@@ -1,4 +1,20 @@
-let hola = "hello world";
-function ellow (){
-    
+function btnMostrar1(){
+    document.getElementById("bntPagos").style.display = "block";
+    document.getElementById("bntTransferencia").style.display = "none";
+    document.getElementById("bntPrestamos").style.display = "none";
+}
+function btnMostrar2(){
+    document.getElementById("bntTransferencia").style.display = "block";
+    document.getElementById("bntPagos").style.display = "none";
+    document.getElementById("bntPrestamos").style.display = "none";
+}
+function btnMostrar3(){
+    document.getElementById("bntPrestamos").style.display = "block";
+    document.getElementById("bntTransferencia").style.display = "none";
+    document.getElementById("bntPagos").style.display = "none";
+}
+
+let btnEnviar = document.getElementById("btnEnviarForm")
+function enviar (btnEnviar){
+    alert("Se ha enviado con Exito!")
 }
