4aab8b3bf0f6e5b785796ae617318d4484a301b6
EliasGalasco <persyxd@gmail.com>
2023-08-29 00:46:42 -0300

    Agregue login

--- style.js
+++ style.js
@@ -18,3 +18,14 @@ let btnEnviar = document.getElementById("btnEnviarForm")
 function enviar (btnEnviar){
     alert("Se ha enviado con Exito!")
 }
+/*----------LOGGIN------ */
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+    
+    // Obtén el valor ingresado por el usuario
+    var username = document.getElementById("username").value;
+    
+    // Modifica el párrafo con el nombre ingresado
+    document.getElementById("welcomeMessage").textContent = "Bienvenido, " + username;
+  });
